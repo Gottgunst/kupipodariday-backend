3c2efed451be3ccf9611a892362be4e692a6af1a
Vladimir Kuleshov <Gottgunst@gmail.com>
2024-06-10 10:02:54 +0300

    feat: Auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfigFactory } from 'src/config/jwt-config';
+import { LocalStrategy } from './strategy/local.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
+  imports: [
+    forwardRef(() => UsersModule),
+    PassportModule,
+    JwtModule.registerAsync({ useClass: JwtConfigFactory }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy, JwtConfigFactory],
+  exports: [AuthService],
 })
 export class AuthModule {}
